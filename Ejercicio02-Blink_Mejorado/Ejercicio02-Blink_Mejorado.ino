4006cb1bab73572a98726c836129fbcd797d0f42
Enrique Crespo <ecrespo@arsys.es>
2017-05-23 11:42:58 +0200

    Blink

---
+++ Ejercicio02-Blink_Mejorado/Ejercicio02-Blink_Mejorado.ino
@@ -0,0 +1,16 @@
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin 13 as an output.
+  Serial.begin(9600);
+  pinMode(13, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
+  Serial.println("Encendido");
+  delay(1000);              // wait for a second
+  digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
+  Serial.println("Apagado");
+  delay(1000);              // wait for a second
+}
