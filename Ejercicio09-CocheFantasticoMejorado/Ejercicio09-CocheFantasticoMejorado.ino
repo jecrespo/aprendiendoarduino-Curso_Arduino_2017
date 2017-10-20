f0a6bd8ffc9880e29e84d34f1ac4430f8df4c62a
Enrique Crespo <aprendiendoarduino@gmail.com>
2017-10-20 20:22:31 +0200

    error en ejercicio 9

--- Ejercicio09-CocheFantasticoMejorado/Ejercicio09-CocheFantasticoMejorado.ino
+++ Ejercicio09-CocheFantasticoMejorado/Ejercicio09-CocheFantasticoMejorado.ino
@@ -16,21 +16,21 @@ int timer = 100;
 
 void setup() {
   // we make all the declarations at once
-  for (count = 0; count < 6; count++) {
+  for (count = 0; count < 5; count++) {
     pinMode(pinArray[count], OUTPUT);
   }
 }
 
 void loop() {
   //timer = analogRead(A0);	//El valor leido por analog read es el temporizador
-  for (count = 0; count < 6; count++) {
+  for (count = 0; count < 5; count++) {
     //timer = analogRead(A0);
     digitalWrite(pinArray[count], HIGH);
     delay(timer);
     digitalWrite(pinArray[count], LOW);
     delay(timer);
   }
-  for (count = 5; count >= 0; count--) {
+  for (count = 4; count >= 0; count--) {
     //timer = analogRead(A0);
     digitalWrite(pinArray[count], HIGH);
     delay(timer);
