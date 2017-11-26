3ae9ecc1ce2e05c22ced20c55d6be62a202d1240
Enrique Crespo <aprendiendoarduino@gmail.com>
2017-11-26 17:48:24 +0100

    Revert "Ejercicio 48"
    This reverts commit 29bb1ed219a16f5cb7d9b7e6d5d8528ca3166026.

--- Ejercicio48-Blink_Plotter_Mejorado/Ejercicio48-Blink_Plotter_Mejorado.ino
+++ Ejercicio48-Blink_Plotter_Mejorado/Ejercicio48-Blink_Plotter_Mejorado.ino
@@ -2,18 +2,19 @@
 void setup() {
   // initialize digital pin 13 as an output.
   Serial.begin(9600);
-
+  pinMode(13, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  
-Serial.println(F("UTF-8 test:"));
-Serial.println(F(" Micro µ µ µ µ µ µ µ µ µ µ"));
-Serial.println(F(" Euro € € € € € € € € € €"));
-Serial.println(F(" (c) © © © © © © © © © ©"));
-Serial.println("Atención");
 
-Serial.println(F("-----------"));
-
+  int segundos = millis() / 1000;
+  if (segundos % 2 == 0) {
+    digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
+    Serial.println("1");
+  }
+  else {
+    digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
+    Serial.println("0");
+  }
 }
